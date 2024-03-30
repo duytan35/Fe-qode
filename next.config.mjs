43250be7140e86acd82c94d21d0f77cc0b077d79
Tan Tran Duy <tantd@algolabs.vn>
2024-03-30 13:32:43 +0700

    fix cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,27 +4,6 @@ const nextConfig = {
     serverUrl: "https://be-qode.vercel.app/api/v1",
     bucketUrl: "https://webnc-2023.s3.ap-southeast-2.amazonaws.com",
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
